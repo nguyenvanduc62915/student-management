f12c268687b104d82e86680766e95b805d2cbbd2
nguyenvanduc62915 <ducsakesmile@gmail.com>
2024-05-02 20:20:39 +0700

    Create Project

---
+++ src/main/java/com/example/studentmanagament/StudentManagamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagamentApplication.class, args);
+    }
+
+}
